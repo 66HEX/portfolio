7edd6e683c0e777e1d6932327f283f093cf67015
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-03-21 15:40:32 +0100

    fix(docs): light mode shiki codeblock visibility

--- src/lib/features/blog/components/ShikiCodeBlock.svelte
+++ src/lib/features/blog/components/ShikiCodeBlock.svelte
@@ -14,14 +14,28 @@
 </script>
 
 <Pre {code} class={className} data-language={lang} {unstyled}>
-  <div class="shiki-theme dark:hidden">
+  <div class="shiki-theme shiki-light">
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html htmlLight}
   </div>
   {#if htmlDark}
-    <div class="shiki-theme hidden dark:block">
+    <div class="shiki-theme shiki-dark">
       <!-- eslint-disable-next-line svelte/no-at-html-tags -->
       {@html htmlDark}
     </div>
   {/if}
 </Pre>
+
+<style>
+  .shiki-dark {
+    display: none;
+  }
+
+  :global(html.dark) .shiki-light {
+    display: none;
+  }
+
+  :global(html.dark) .shiki-dark {
+    display: block;
+  }
+</style>
